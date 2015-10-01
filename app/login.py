895f04c06c29122287cd1936b50fbcc67c54dda2
Yuyang Zhang <695441493@qq.com>
2015-10-01 19:01:47 +0800

    move test.html to index.html and move the db part to sqlite3

--- app/login.py
+++ app/login.py
@@ -1,20 +1,4 @@
-import sqlite3
-from flask import g
-
-DATABASE = './db/dataBase.sqlite3'
-
-def get_db():
-	db = getattr(g , '_database' , None)
-	if db is None:
-		db = g._database = connect_to_database()
-	return db
-
-
-def close_connection(exception):
-	db = getattr(g, '_database', None)
-	if db is not None:
-		db.close()
-
+import sqlite3_db
 def login(username , password):
 	query = 'select password from user where username = ?'
 	cur = get_db().execute(query , username)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template request
+from flask import render_template , request
 
 
-@app.route('/')
-@app.route('/login',method=['GET' , 'POST'])
+@app.route('/' , methods=['GET' , 'POST'])
+@app.route('/login', methods=['GET' , 'POST'])
 def login():
 	if request.method == 'GET':
-		return render_template("test.html")
+		return render_template("index.html")
 	elif request.method == 'POST':
-		
+		if request.form['username']:
+			return 'Success'
+		else:
+			return 'Fail'
