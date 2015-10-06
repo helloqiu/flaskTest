8a78d5df2dffd2f64fe329a6b29ca081baa718df
Yuyang Zhang <695441493@qq.com>
2015-10-06 14:21:32 +0800

    use plugin to auto format.fix #5

--- app/getQuestion.py
+++ app/getQuestion.py
@@ -1,16 +1,19 @@
 import sqlite3_db
 import string
-def get_question(kind , titleOnly=True , num = '0'):
-	if titleOnly:
-		query='select title,score from question where kind = %s order by score desc' %kind
-		cur = sqlite3_db.connect_db().execute(query)
-		rv = cur.fetchall()
-		cur.close()
-		return rv
-	else:
-		query='select title,content,answer,kind,score from question where num = %d order by score desc' %string.atoi(num)
-		cur = sqlite3_db.connect_db().execute(query)
-		rv = cur.fetchall()
-		cur.close()
-		rv = rv[0]
-		return rv
+
+
+def get_question(kind, titleOnly=True, num='0'):
+    if titleOnly:
+        query = 'select title,score from question where kind = %s order by score desc' % kind
+        cur = sqlite3_db.connect_db().execute(query)
+        rv = cur.fetchall()
+        cur.close()
+        return rv
+    else:
+        query = 'select title,content,answer,kind,score from question where num = %d order by score desc' % string.atoi(
+            num)
+        cur = sqlite3_db.connect_db().execute(query)
+        rv = cur.fetchall()
+        cur.close()
+        rv = rv[0]
+        return rv
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template , request
+from flask import render_template, request
 import getQuestion
 import user
 
@@ -8,58 +8,63 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-@app.route('/' , methods=['GET' , 'POST'])
-@app.route('/login', methods=['GET' , 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login_view():
-	if request.method == 'GET':
-		return render_template("index.html")
-	elif request.method == 'POST':
-		if request.form['username']:
-			if user.login(request.form['username'] , request.form['password']):
-				return user_dash_view()
-			else:
-				return render_template("index.html" , error=True)
-		else:
-			return 'Fail'
-@app.route('/register', methods=['GET' , 'POST'])
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        if request.form['username']:
+            if user.login(request.form['username'], request.form['password']):
+                return user_dash_view()
+            else:
+                return render_template("index.html", error=True)
+        else:
+            return 'Fail'
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register_view():
-	if request.method == 'GET':
-		return render_template('register.html')
-	elif request.method == 'POST':
-		if request.form['password'] != request.form['password-repeat']:
-			return render_template('register.html' , error='passwords are not same')
-		else:
-			if user.register(request.form['username'] , request.form['password']):
-				return 'success'
-			else:
-				return render_template('register.html' , error='the username has been registered')
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        if request.form['password'] != request.form['password-repeat']:
+            return render_template('register.html', error='passwords are not same')
+        else:
+            if user.register(request.form['username'], request.form['password']):
+                return 'success'
+            else:
+                return render_template('register.html', error='the username has been registered')
 
-@app.route('/user_dash', methods=['GET' , 'POST'])
+
+@app.route('/user_dash', methods=['GET', 'POST'])
 def user_dash_view():
-	data=user.rank()
-	if request.method == 'GET':
-		return render_template('user_dashboard.html' , users=data )
-	if request.method == 'POST':
-		return render_template('user_dashboard.html' , users=data )
+    data = user.rank()
+    if request.method == 'GET':
+        return render_template('user_dashboard.html', users=data)
+    if request.method == 'POST':
+        return render_template('user_dashboard.html', users=data)
 
-@app.route('/question/<int:kind>', methods=['GET' , 'POST'])
+
+@app.route('/question/<int:kind>', methods=['GET', 'POST'])
 def question(kind):
-	data=getQuestion.get_question('%d' %kind )
-	if request.method == 'GET':
-		return render_template('question_list.html' , question_kind='题目' + '%d' %kind, questions = data , kind=kind)
+    data = getQuestion.get_question('%d' % kind)
+    if request.method == 'GET':
+        return render_template('question_list.html', question_kind='题目' + '%d' % kind, questions=data, kind=kind)
 
-@app.route('/question/id/<int:id>', methods=['GET' , 'POST'])
+
+@app.route('/question/id/<int:id>', methods=['GET', 'POST'])
 def show_questions(id):
-	data=getQuestion.get_question('' , False , '%d' %id)
-	if request.method == 'GET':
-		return render_template('question.html' , question_title=data[0] , question_content=data[1] , kind=data[3])
-	if request.method == 'POST':
-		if user.login(request.form['username'] , request.form['password']):
-			if data[2] != unicode(request.form['answer']):
-				return render_template('question.html' , question_title=data[0] , question_content=data[1] , kind=data[3] , error=True)
-			else:
-				user.add_score(request.form['username'],data[4])
-				return render_template('question.html' , question_title=data[0] , question_content=data[1] , kind=data[3] , success=True)
-		else:
-			return render_template('question.html' , question_title=data[0] , question_content=data[1] , kind=data[3] , loginError=True)
-		
+    data = getQuestion.get_question('', False, '%d' % id)
+    if request.method == 'GET':
+        return render_template('question.html', question_title=data[0], question_content=data[1], kind=data[3])
+    if request.method == 'POST':
+        if user.login(request.form['username'], request.form['password']):
+            if data[2] != unicode(request.form['answer']):
+                return render_template('question.html', question_title=data[0], question_content=data[1], kind=data[3], error=True)
+            else:
+                user.add_score(request.form['username'], data[4])
+                return render_template('question.html', question_title=data[0], question_content=data[1], kind=data[3], success=True)
+        else:
+            return render_template('question.html', question_title=data[0], question_content=data[1], kind=data[3], loginError=True)
