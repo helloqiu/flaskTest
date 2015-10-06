2f9dca3d475c312ec7e20478433e807fe9874fef
Yuyang Zhang <695441493@qq.com>
2015-10-06 22:34:47 +0800

    use flask-script and write a shell

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 from app import app
-app.run(debug=True, host='0.0.0.0')
+from flask.ext.script import Manager
+
+
+#app.run(debug=True, host='0.0.0.0')
+manager = Manager(app)
+if __name__ == '__main__':
+	manager.run()
