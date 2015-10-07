a4a9d39d541ad11bac5c5be14f6134f47eb10990
Yuyang Zhang <695441493@qq.com>
2015-10-07 11:03:39 +0800

    decide not to use the flask-sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloqiu string'
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    #os.path.join(basedir, 'data.sqlite')
+#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 bootstrap = Bootstrap(app)
 
 from app import views
--- app/sqlite3_db.py
+++ app/sqlite3_db.py
@@ -1,16 +1,42 @@
 import sqlite3
 from flask import g
 from app import app
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 DATABASE = 'app/dataBase.sqlite3'
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#db = SQLAlchemy(app)
 
+
+'''class User(db.Model):
+    __tablename__ = 'users'
+    username = db.Column(db.String(64), unique=True,
+                         primary_key=True, index=True)
+    password = db.Column(db.String)
+    score = db.Column(db.Integer, default=0)
+    achievement = db.Column(db.Unicode)
+
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, unique=True, primary_key=True, index=True)
+    title = db.Column(db.Unicode)
+    content = db.Column(db.UnicodeText)
+    score = db.Column(db.Integer)
+    answer = db.Column(db.Unicode)
+    kind = db.Column(db.Integer)
+'''
 def connect_db():
     return sqlite3.connect(DATABASE)
+
 
 @app.before_request
 def before_request():
     g.db = connect_db()
 
+
 @app.teardown_request
 def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
+
