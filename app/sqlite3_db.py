9cbe4be82895488b2d08d6034f76a9e863948f28
Yuyang Zhang <695441493@qq.com>
2015-10-15 14:41:36 +0800

    correct some mistakes

--- app/sqlite3_db.py
+++ app/sqlite3_db.py
@@ -10,12 +10,12 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_kry=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password = db.Column(db.String(64))
     score = db.Column(db.Integer, default=0)
     achievement = db.Column(db.String)
-    email = sb.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
     def __repr__(self):
         return 'User %r' % self.username
